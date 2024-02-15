6c178ed172c1e372c3d7daf5c1cfd0f46034f041
David Alvarado <davidaeg90@gmail.com>
2024-02-15 12:29:08 -0600

    updates

--- src/modules/Products/Products.tsx
+++ src/modules/Products/Products.tsx
@@ -1,8 +1,8 @@
 import { Panel } from 'primereact/panel';
 import { useProductApi } from '../../shared/datasources/products/products-api/useProductApi.hook';
 import { useEffect } from 'react';
-
 import { ProductCard } from '../../shared/components/productCard/ProductCard';
+
 export const Products = () => {
   const { currentPoducts, getAllProducts, error } = useProductApi();
 
@@ -18,7 +18,7 @@ export const Products = () => {
     <Panel header="Products">
       <div className="grid justify-content-center">
         {currentPoducts.map((product) => (
-          <div className="col-12 md:col-6 lg:col-3 ">
+          <div className="col-12 md:col-6 lg:col-3 " key={product.productId}>
             {ProductCard(product)}
           </div>
         ))}
