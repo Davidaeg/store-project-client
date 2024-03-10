caf00a063b694e480dc84772f6a28e2b2d4c2920
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-09 23:06:00 -0600

    Programación de un Banner para Home
    Se incluyo un componente Banner, sus estilos y la imagen del banner. Además se solucionó un bug relacionado a que un estilo de height en el Panel estaba generando que se solaparan los componentes.

---
+++ src/modules/home/components/banner/banner.tsx
@@ -0,0 +1,14 @@
+import banner from '/public/images/banner.webp';
+import './banner.styles.css'; 
+
+export const Banner: React.FC = () => {
+  return (
+    <div className="banner_image">
+      <img src={banner} alt="Eleva tu estilo con Fashion Loft: confianza y calidad en cada compra." />
+        <div className="banner_text">
+          <h1>¡Bienvenido a Fashion Loft!</h1>
+          <p>Eleva tu estilo con Fashion Loft: confianza y calidad en cada compra.</p>      
+        </div>
+    </div>
+  );
+};
