67da9d8c7efa5477b47adcbb3cc68b09b40dbde7
Andre Rodriguez <joserox1218@gmail.com>
2024-03-15 17:24:54 -0600

    Implemented price filters on product page

--- src/modules/Products/Products.tsx
+++ src/modules/Products/Products.tsx
@@ -1,10 +1,39 @@
 import { Panel } from 'primereact/panel';
-import { useGetAllProducts } from '../../shared/datasources/products/products-api/useGetAllProducts.hook';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { ProductCard } from '../../shared/components/productCard/ProductCard';
+import '../Products/ProductsStyles.css';
+import { useProductFilter } from '../../shared/datasources/products/products-api/useProductFilter.hook';
+import { Dropdown, DropdownChangeEvent } from 'primereact/dropdown';
+import { Orders, options } from './OrdersTypes.enum';
 
 export const Products = () => {
-  const { currentPoducts, getAllProducts, error } = useGetAllProducts();
+  const {
+    filterProductsByName,
+    getAllProducts,
+    getAllProductsDesc,
+    getAllProductsAsc,
+    currentPoducts,
+    error
+  } = useProductFilter();
+
+  const [order, setOrder] = useState<Orders>(Orders.Default);
+
+  const handleOrder: Record<string, () => void> = {
+    [Orders.Ascendent]: () => {
+      getAllProductsAsc();
+    },
+    [Orders.Descendent]: () => {
+      getAllProductsDesc();
+    },
+    [Orders.Default]: () => {
+      getAllProducts();
+    }
+  };
+
+  const handleOrderChange = (e: DropdownChangeEvent) => {
+    setOrder(e.value);
+    handleOrder[e.value]();
+  };
 
   useEffect(() => {
     getAllProducts()
@@ -17,11 +46,20 @@ export const Products = () => {
   return (
     <Panel header="Products">
       <div className="grid justify-content-center">
-        {currentPoducts.map((product) => (
-          <div
-            className="col-12 md:col-6 lg:col-3 xl:col-2"
-            key={product.productId}
-          >
+        <div className="card flex justify-content-center">
+          <Dropdown
+            value={order}
+            onChange={handleOrderChange}
+            options={options}
+            optionLabel="label"
+            placeholder="Select a City"
+            className="w-full md:w-14rem"
+          />
+        </div>
+      </div>
+      <div className="grid justify-content-center products-display">
+        {currentPoducts.map((product: any) => (
+          <div className="col-12 md:col-6 lg:col-3" key={product.productId}>
             {ProductCard(product)}
           </div>
         ))}
---
+++ src/shared/datasources/products/products-api/useFilterByName.hook.ts
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import { Product } from '../products.types';
+import storeService from '../../store-service/storeService';
+
+export const useFilterByName = () => {
+  const [errorByName, setError] = useState<string>('');
+  const [currentPoductsByName, setCurrentProducts] = useState<Product>();
+
+  const filterProductsByName = async (name: String) => {
+    try {
+      await storeService
+        .get(`/products/search/${name}`)
+        .then((resp) => {
+          setCurrentProducts(resp.data as Product);
+        })
+        .catch((e) => {
+          setError(e.message);
+        });
+    } catch (e: any) {
+      console.log(e);
+      setError(e.message);
+    }
+  };
+
+  return {
+    filterProductsByName,
+    currentPoductsByName,
+    errorByName
+  };
+};
