7ec76750650612c216381c80f95d0a39e132eff1
David Alvarado <davidaeg90@gmail.com>
2024-04-06 09:46:35 -0600

    pushing product fixing

--- src/modules/Products/Products1.tsx
+++ src/modules/Products/Products1.tsx
@@ -1,7 +1,8 @@
 import { Panel } from 'primereact/panel';
 import { useEffect, useState } from 'react';
 import { ProductCard } from '../../shared/components/productCard/ProductCard';
-import '../Products/ProductsStyles.css';
+import '../products/ProductsStyles.css';
+
 import { useProductFilter } from '../../shared/datasources/products/products-api/useProductFilter.hook';
 import { Dropdown, DropdownChangeEvent } from 'primereact/dropdown';
 import { Orders, options } from './OrdersTypes.enum';
